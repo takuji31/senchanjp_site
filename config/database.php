f22f826e9490a6faa17a6692c0883aea93187058
西林 拓志 <takuji@takujimac.local>
2010-03-22 15:02:24 +0900

    Cakeを入れた。

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '4990165',
+		'database' => 'senchanjp',
+		'encoding' => 'utf8'
+	);
+}
+?>
