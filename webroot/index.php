e22457f2fedab717bfe8772cf9eb211be88cc07c
Nishibayashi Takuji <takuji@senchan.jp>
2010-07-27 08:08:09 +0900

    VPSの環境に合わせた

--- webroot/index.php
+++ webroot/index.php
@@ -50,7 +50,7 @@
  *
  */
 	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		define('CAKE_CORE_INCLUDE_PATH',  dirname(dirname(dirname(dirname(__FILE__)))).'/project/cake');
+		define('CAKE_CORE_INCLUDE_PATH',  ROOT .'/cake');
 	}
 
 /**
